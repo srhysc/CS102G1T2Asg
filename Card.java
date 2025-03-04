24d4cb632e9e7ef350691ded25097d6826696a51
srhysc <srhyschua@gmail.com>
2025-03-04 14:56:42 +0800

    Added Card and Deck Classes

---
+++ Card.java
@@ -0,0 +1,41 @@
+public class Card{
+
+    public int value;
+    public String colour;
+
+    
+
+    public Card(int value, String colour) {
+        this.value = value;
+        this.colour = colour;
+    }
+
+
+
+    public int getValue() {
+        return value;
+    }
+
+
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+
+
+    public String getColour() {
+        return colour;
+    }
+
+
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    
+
+
+
+}
